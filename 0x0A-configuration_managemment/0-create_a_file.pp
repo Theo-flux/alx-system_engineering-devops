2abb553797e9d711b25477cd4ab140b66c343edb
Theo <ekunnusitheophilus@gmail.com>
2022-10-14 12:38:26 +0100

    Folder renamed

--- 0x0A-configuration_managemment/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-# create a file in /tmp.
-file { '/school':
-  ensure  => file,
-  path    => '/tmp',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
-}
