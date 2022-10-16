f7ef422b8e9ac7c2b760d719c8bff2bbc5475133
Theo <ekunnusitheophilus@gmail.com>
2022-10-16 15:52:24 +0100

    puppet fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,16 @@
-# Client configuration file
+#client configuration file
 include stdlib
 
 file_line {'Ssh private key file config':
   eensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no',
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
   replace => true,
 }
 
 file_line {'Delare identity file':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => '     IdentityFile ~/.ssh/school',
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '     IdentityFile ~/.ssh/school',
   replace => true,
 }
