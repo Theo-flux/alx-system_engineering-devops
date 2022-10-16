58d5d6b0fbcc294a46eb1c0ca10ded468f40c958
Theo <ekunnusitheophilus@gmail.com>
2022-10-16 15:45:37 +0100

    Client config file using puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+# Client configuration file
+include stdlib
+
+file_line {'Ssh private key file config':
+  eensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line {'Delare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
+}
