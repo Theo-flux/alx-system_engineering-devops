5ec29216d82c24182ada29ff4647af01f42ae87a
Theo <ekunnusitheophilus@gmail.com>
2022-11-17 11:36:47 +0100

    Hot Fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,38 +1,14 @@
 #!/usr/bin/python3
-"""
-python script that returns todo list progress from
-an api
-"""
-from sys import argv
+"""Returns to-do list information for a given employee ID."""
 import requests
+import sys
 
 if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    id = argv[1]
-    baseUrl = 'https://jsonplaceholder.typicode.com'
-    urlForTodo = '{}/todos?userId={}'.format(baseUrl, id)
-    urlForUser = '{}/users?id={}'.format(baseUrl, id)
-    name = ""
-    completed_tasks = []
-    total_tasks = 0
-
-    with requests.get(urlForUser) as res:
-        result = res.json()
-        name = result[0].get('name')
-
-    with requests.get(urlForTodo) as res:
-        result = res.json()
-        total_tasks = len(result)
-
-        for k in range(0, len(result)):
-            if result[k].get('completed') is True:
-                completed_tasks.append(result[k].get('title'))
-
-    def format_output(name, completed, total):
-        print('Employee {} is done with tasks({}/{}):'
-              .format(name, len(completed), total))
-
-        for i in completed:
-            print('\t {}'.format(i))
-
-    format_output(name, completed_tasks, total_tasks)
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
