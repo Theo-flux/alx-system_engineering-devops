0719ad2019f7c80207bcb27f19a81e8479c509ad
Theo <ekunnusitheophilus@gmail.com>
2022-12-26 16:30:41 +0100

    [feat]: get total subscribers in a subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+a function that queries the Reddit API and
+returns the number of subscribers (not active users, total subscribers)
+for a given subreddit. If an invalid subreddit is given,
+the function should return 0.
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+
+    base_url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': '0x16-api_advanced_project'}
+    r = requests.get(base_url, headers=headers).json()
+
+    if r.get('data') is None:
+        return 0
+
+    subscribers = r.get('data').get('subscribers')
+    return subscribers
