c1a7e3ea833191ba9590ede3ffde76838ed1722e
Theo <ekunnusitheophilus@gmail.com>
2022-12-26 18:33:03 +0100

    [fix]

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,38 +1,31 @@
-#!/usr/python3
-"""
-a recursive function that queries the Reddit API and
-returns a list containing the titles of all hot articles
-for a given subreddit. If no results are found for the given subreddit,
-the function should return None.
-"""
+#!/usr/bin/python3
+"""Contains recurse function"""
 import requests
 
 
 def recurse(subreddit, hot_list=[], after="", count=0):
-    if subreddit is None or type(subreddit) is not str:
+    """Returns a list of titles of all hot posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
         return None
 
-    base_url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
-    headers = {'User-Agent': '0x16-api_advanced_project'}
-    params = {"after": after, "count": count, "limit": 100}
-    r = requests.get(
-        base_url,
-        headers=headers,
-        params=params,
-        # allow_redirects=False
-        ).json()
-
-    if r.get('data') is None:
-        return None
-
-    after = r.get('data').get('after')
-    count += r.get('data').get('dist')
-    children = r.get('data').get('children')
-
-    for child in children:
-        hot_list.append(child.get('title'))
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get("title"))
 
     if after is not None:
         return recurse(subreddit, hot_list, after, count)
-    else:
-        return hot_list
+    return hot_list
