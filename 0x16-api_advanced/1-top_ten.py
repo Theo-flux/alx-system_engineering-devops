908503a4e67f214ead040c011a64ecf32522e0cd
Theo <ekunnusitheophilus@gmail.com>
2022-12-26 17:01:23 +0100

    [feat]: get hot top ten titles from a subreddit.

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+function that queries the Reddit API and
+prints the titles of the first 10 hot posts
+listed for a given subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    if subreddit is None or type(subreddit) is not str:
+        print('None')
+        return
+
+    base_url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+    headers = {'User-Agent': '0x16-api_advanced_project'}
+    r = requests.get(base_url, headers=headers).json()
+
+    if r.get('data') is None:
+        print('None')
+        return
+
+    i = 0
+    data = r.get('data').get('children')
+
+    for i in range(0, 10):
+        print(data[i].get('data').get('title'))
+        i += 1
